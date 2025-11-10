da76cd58004bf381d7ea6dc1bcfc9df3f32cb866
Streams-Eth <swapstream.finance@gmail.com>
2025-11-10 02:21:16 +0000

    Revert "chore(ui): add client error boundary and wrap Providers to reveal client runtime errors instead of blank page"
    This reverts commit fcea25a587f71c6babaee49b62555a8317d9abf8.

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,12 +2,7 @@
 
 import React from "react"
 import { Web3Provider } from "@/components/web3-provider"
-import ClientErrorBoundary from "@/components/client-error-boundary"
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return (
-    <ClientErrorBoundary>
-      <Web3Provider>{children}</Web3Provider>
-    </ClientErrorBoundary>
-  )
+  return <Web3Provider>{children}</Web3Provider>
 }
